ad4907fe243ca90c5f53fb0cd4ea3e798aef5970
centuryhopper <43508492+centuryhopper@users.noreply.github.com>
2024-07-07 18:42:11 -0400

    update

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,42 +1,43 @@
 using DotnetUserManagementSystem.Contexts;
+using DotnetUserManagementSystem.Utilities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotnetUserManagementSystem.Controllers
+namespace DotnetUserManagementSystem.Controllers;
+
+[Authorize(Roles = Constants.ADMIN)]
+public class UsersController : Controller
 {
-    public class UsersController : Controller
+    private readonly UserManager<ApplicationUser> userManager;
+
+    public UsersController(UserManager<ApplicationUser> userManager)
     {
-        private readonly UserManager<ApplicationUser> userManager;
-        //Server=ep-shy-boat-a5z9pcbn.us-east-2.aws.neon.tech;Database=UserManagement;User Id=neondb_owner;Password=NSFWkL9Zwb6f;Port=5432
-
-        public UsersController(UserManager<ApplicationUser> userManager)
-        {
-            this.userManager = userManager;
-        }
-        
-        public async Task<ActionResult> Index()
-        {
-            var users = userManager.Users.AsEnumerable().ToList();
-            Dictionary<string,IEnumerable<string>> userRoles = new();
-            foreach (var user in users)
-            {
-                userRoles.Add(user.Id, await userManager.GetRolesAsync(user));
-            }
-            ViewBag.UserRoles = userRoles;
-            ViewBag.Users = users;
-
-            return View();
-        }
-
-        public async Task<ActionResult> AddUser()
-        {
-            return View();
-        }
-
-        public async Task<ActionResult> RemoveUser()
-        {
-            return View();
-        }
-
+        this.userManager = userManager;
     }
+    
+    public async Task<ActionResult> Index()
+    {
+        var users = userManager.Users.AsEnumerable().ToList();
+        Dictionary<string,IEnumerable<string>> userRoles = new();
+        foreach (var user in users)
+        {
+            userRoles.Add(user.Id, await userManager.GetRolesAsync(user));
+        }
+        ViewBag.UserRoles = userRoles;
+        ViewBag.Users = users;
+
+        return View();
+    }
+
+    public async Task<ActionResult> AddUser()
+    {
+        return View();
+    }
+
+    public async Task<ActionResult> RemoveUser()
+    {
+        return View();
+    }
+
 }
